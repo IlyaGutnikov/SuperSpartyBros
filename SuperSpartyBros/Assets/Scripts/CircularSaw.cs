2b662a58d027203d3950b31ef36d363bfbc38bb6
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-11-15 20:02:02 +0300

    Add circle saw and heart (external life)

---
+++ SuperSpartyBros/Assets/Scripts/CircularSaw.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircularSaw : MonoBehaviour {
+
+	public int damageAmount = 10;
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if ((collision.tag == "Player") )
+		{
+			CharacterController2D player = collision.gameObject.GetComponent<CharacterController2D>();
+			if (player.playerCanMove) {
+
+				// apply damage to the player
+				player.ApplyDamage (damageAmount);
+
+			}
+		}
+	}
+}
